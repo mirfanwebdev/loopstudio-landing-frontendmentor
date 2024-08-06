bd1cad7dfc14c9256115b9883346f5bbe9dbe034
m.irfan <m.irfanwebdev@gmail.com>
2024-08-06 17:57:57 +0700

    setup font and reset content

---
+++ src/components/BaseMenu.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue";
+
+const list = ref([
+    {
+        id: 1,
+        name: 'About'
+    },
+    {
+        id: 2,
+        name: 'Careers'
+    },
+    {
+        id: 3,
+        name: 'Events',
+    },
+    {
+        id: 4,
+        name: 'Products',
+    },
+    {
+        id: 5,
+        name: 'Support'
+    }
+]);
+</script>
+
+<template>
+    <div v-for="item in list" :key="item.id">
+        <a>{{ item.name }}</a>
+    </div>
+</template>
+
+<style scoped>
+a {
+    text-decoration: none;
+}
+</style>
