538f7dcef9867b4d9540544a5635b7b7b1775eab
m.irfan <m.irfanwebdev@gmail.com>
2024-08-07 16:08:15 +0700

    add main section and product items

---
+++ src/components/Interactive.vue
@@ -0,0 +1,34 @@
+<script setup>
+</script>
+<template>
+<section>
+    <img src="../assets/mobile/image-interactive.jpg" alt="interactive">
+    <div class="container-text">
+      <h2>The leader in interactive VR</h2>
+      <p>Founded in 2011, Loopstudios has been producing world-class virtual reality 
+  projects for some of the best companies around the globe. Our award-winning 
+  creations have transformed businesses through digital experiences that bind 
+  to their brand.</p>
+    </div>
+</section>
+</template>
+<style scoped>
+img {
+    width: 100%;
+}
+h2 {
+    font-family: var(--josefin-sans);
+    font-weight: 300;
+    text-transform: uppercase;
+    font-size: 2.25rem;
+    margin: 1.5rem 0;
+}
+.container-text {
+  padding: 1.5rem 1.5rem 0 1.5rem;
+  text-align: center;
+}
+p {
+  line-height: 1.6;
+  color: var(--dark-gray);
+}
+</style>
