0ecf95cec921741447e5ceef8e95b2e5c5b229e2
m.irfan <m.irfanwebdev@gmail.com>
2024-08-11 13:58:10 +0700

    add responsive design to footer and fix padding on products

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,14 +5,18 @@ import BaseSocial from './BaseSocial.vue';
 
 <template>
     <footer>
-        <img class="logo" src="../assets/logo.svg" alt="logo"/>
-        <div class="flex">
-            <BaseMenu/>
+        <div class="flex-1">
+            <img class="logo" src="../assets/logo.svg" alt="logo"/>
+            <div class="flex">
+                <BaseMenu/>
+            </div>
         </div>
-        <div class="social">
-            <BaseSocial/>
+        <div class="flex-2">
+            <div class="social">
+                <BaseSocial/>
+            </div>
+            <p class="copyright">© 2021 Loopstudios. All rights reserved.</p>
         </div>
-        <p class="copyright">© 2021 Loopstudios. All rights reserved.</p>
     </footer>
 </template>
 
@@ -36,11 +40,36 @@ footer {
     gap: 1rem;
     padding: 1.5rem 0;
 }
+.flex-1, .flex-2 {
+    display: flex;
+    flex-direction: column;
+    gap: 1.25rem;
+    align-items: center;
+}
 .social {
     display: flex;
     gap: 1rem;
 }
 .copyright {
     color: var(--dark-gray);
+}
+
+@media screen and (min-width: 768px) {
+    footer {
+        flex-direction: row;
+        justify-content: space-between;
+        padding: 2.5rem 12rem;
+    }
+    .flex {
+        flex-direction: row;
+        gap: 1.5rem;
+        padding: 0;
+    }
+    .flex-1 {
+        align-items: flex-start;
+    }
+    .flex-2 {
+        align-items: flex-end;
+    }
 }
 </style>
